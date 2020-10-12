0482c9a725229fa7abf19a4ce8d6ae48714292b8
Ryan Nuqui <ryannuqui@berkeley.edu>
2020-10-12 12:06:37 -0700

    Added test files

--- week8/student/BinaryTreeTest.java
+++ week8/student/BinaryTreeTest.java
@@ -21,9 +21,9 @@ public class BinaryTreeTest {
     @Test
     public void testHeightOneChild() {
         /** Test:
-                 61
-                /
-               70
+         61
+         /
+         70
          */
 
         Node<Integer> root = new Node<>();
@@ -43,9 +43,9 @@ public class BinaryTreeTest {
         assertEquals(expected, actual);
 
         /** Test:
-                 61
-                    \
-                    70
+         61
+         \
+         70
          */
 
         root.right = root.left;
@@ -59,11 +59,11 @@ public class BinaryTreeTest {
     @Test
     public void testHeightComplete() {
         /** Test:
-                            61
-                        /         |
-                       70         88
-                     /   |      /   |
-                   161   162   168  169
+         61
+         /         |
+         70         88
+         /   |      /   |
+         161   162   168  169
          */
 
         //Grandchildren
@@ -84,6 +84,119 @@ public class BinaryTreeTest {
 
         int expected = 2;
         int actual = binaryTree.height();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testIsBalancedLeaf() {
+        /** Test:
+         * 61
+         */
+
+        Node<Integer> root = new Node<>(61);
+
+        BinaryTree<Integer> binaryTree = new BinaryTree<>();
+        binaryTree.root = root;
+
+        boolean expected = true;
+        boolean actual = binaryTree.isBalanced();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testIsBalancedOneChild() {
+        /** Test:
+         61
+         /
+         70
+         */
+
+        Node<Integer> root = new Node<>();
+        Node<Integer> child = new Node<>();
+
+        root.value = 61;
+        child.value = 70;
+
+        root.left = child;
+
+
+        BinaryTree<Integer> binaryTree = new BinaryTree<>();
+        binaryTree.root = root;
+
+        boolean expected = true;
+        boolean actual = binaryTree.isBalanced();
+        assertEquals(expected, actual);
+
+        /** Test:
+         61
+         \
+         70
+         */
+
+        root.right = root.left;
+        root.left = null;
+
+        expected = true;
+        actual = binaryTree.isBalanced();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testIsBalancedComplete() {
+        /** Test:
+         61
+         /         |
+         70         88
+         /   |      /   |
+         161   162   168  169
+         */
+
+        //Grandchildren
+        Node<Integer> grandChild1 = new Node<>(161);
+        Node<Integer> grandChild2 = new Node<>(162);
+        Node<Integer> grandChild3 = new Node<>(168);
+        Node<Integer> grandChild4 = new Node<>(169);
+
+        //Children
+        Node<Integer> leftChild = new Node<>(70, grandChild1, grandChild2);
+        Node<Integer> rightChild = new Node<>(88, grandChild3, grandChild4);
+
+        //Root
+        Node<Integer> root = new Node<>(61, leftChild, rightChild);
+
+        BinaryTree<Integer> binaryTree = new BinaryTree<>();
+        binaryTree.root = root;
+
+        boolean expected = true;
+        boolean actual = binaryTree.isBalanced();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testisBalancedNotComplete() {
+        /** Test:
+         61
+         /
+         70
+         /   |
+         161   162
+         */
+
+        //Grandchildren
+        Node<Integer> grandChild1 = new Node<>(161);
+        Node<Integer> grandChild2 = new Node<>(162);
+
+        //Child
+        Node<Integer> leftChild = new Node<>(70, grandChild1, grandChild2);
+
+        //Root
+        Node<Integer> root = new Node<>(61, leftChild, null);
+
+        BinaryTree<Integer> binaryTree = new BinaryTree<>();
+        binaryTree.root = root;
+
+        boolean expected = false;
+        boolean actual = binaryTree.isBalanced();
         assertEquals(expected, actual);
     }
 }
